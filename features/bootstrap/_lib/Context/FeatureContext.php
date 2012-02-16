891be4d82c69903dff4985b7223ce6d99215cc14
Angel Koilov <angel.koilov@gmail.com>
2012-02-16 21:58:48 +0200

    comments

--- features/bootstrap/_lib/Context/FeatureContext.php
+++ features/bootstrap/_lib/Context/FeatureContext.php
@@ -3,8 +3,10 @@ namespace ptbf;
 
 /**
  * Description of FeatureContext
- *
- * @author potaka
+ * Here we will override some mink methods
+ * basicly all CSS selectors wich return 1 element, should have founded exaclity 1 element
+ * 
+ * @author Angel Koilov <angel.koilov@gmail.com>
  */
 
 require __DIR__.'/../_classLoader.php';
