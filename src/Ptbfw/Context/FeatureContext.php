c940f69bd60af344dc0fa21b68d6f02b4b4546dd
po_taka <angel.koilov@gmail.com>
2015-01-16 12:15:55 +0200

    fix changes to behat v3

--- src/Ptbfw/Context/FeatureContext.php
+++ src/Ptbfw/Context/FeatureContext.php
@@ -2,7 +2,8 @@
 
 namespace Ptbfw\Context;
 
-use Behat\Behat\Event\SuiteEvent;
+use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 
 /**
  * Description of FeatureContext
@@ -15,47 +16,11 @@ class FeatureContext extends \Behat\MinkExtension\Context\MinkContext implements
 
 	protected static $mink;
 	protected static $options = array();
-	private $contextDir = null;
 
 	function __construct($options) {
 		self::$options = $options;
-
-		// load all context files
-		if (isset($options['context_dir'])) {
-			if (!isset($options['context_dir_path'])) {
-				throw new \Exception('set option "context_dir_base_path" with full path to context dir');
-			}
-			$this->contextDir = rtrim($options['context_dir_path'], '/');
-			
-			foreach ($options['context_dir'] as $nameSpace => $directory) {
-				if (!preg_match('/^\\\\/', $nameSpace)) {
-					$nameSpace = '\\' . $nameSpace;
-				}
-
-				if (!preg_match('/\\\\$/', $nameSpace)) {
-					$nameSpace = $nameSpace . '\\';
-				}
-
-				$finder = new \Symfony\Component\Finder\Finder();
-				$contextDir = $this->contextDir . '/' . $directory;
-				$contextDirReal = realpath($contextDir);
-				if (!$contextDirReal) {
-					throw new \Exception("{$contextDir} do not exist");
-				}
-				foreach ($finder->files()->name('*.php')->sortByName()->in($contextDirReal) as $file) {
-					/* @var $file \Symfony\Component\Finder\SplFileInfo */
-					$contextName = preg_replace('/\.php$/', '', $file->getFilename());
-					$relativePath = preg_replace('/^' . preg_quote($contextDirReal, '/') . '/', '', $file->getRealPath());
-					$relativePath = preg_replace('#/#', '\\', $relativePath);
-					$relativePath = ltrim($relativePath, '\\');
-					$contextName = preg_replace('/\.php$/', '', $relativePath);
-					$contextFullClassName = $nameSpace . $contextName;
-					$this->useContext($contextFullClassName, new $contextFullClassName($this));
-				}
-			}
-		}
 	}
-
+    
 	public function getMink() {
 		return self::getMinkStatic();
 	}
@@ -72,28 +37,19 @@ class FeatureContext extends \Behat\MinkExtension\Context\MinkContext implements
 	/**
 	 * @BeforeSuite
 	 */
-	public static function prepareSuite(SuiteEvent $event) {
-		self::$options = $event->getContextParameters();
+	public static function prepareSuite(BeforeSuiteScope $scope) {
+        $event = $scope->getSuite()->getSettings();
+		self::$options = $event;
 		$options = self::$options;
-	}
-
-	/**
-	 * override default mink hook
-	 * we moved this to before scenario step
-	 * because you can stop/change some sessions during scenario
-	 */
-	public function prepareMinkSessions($event) {
-		
 	}
 
 	/**
 	 * 
 	 * Restore mink sessions from config
 	 * 
-	 * @BeforeScenario 
-	 * @param \Behat\Behat\Event\ScenarioEvent $event
+	 * @BeforeScenario
 	 */
-	public function before($event) {
+	public function before(BeforeScenarioScope $scope) {
 		$this->sessionRestart();
 	}
 
@@ -117,8 +73,7 @@ class FeatureContext extends \Behat\MinkExtension\Context\MinkContext implements
 		 * firefox
 		 */
 		if (preg_match('/(?:(mink|ie|firefox|chrom||safari|internet|explorer|opera))/i', $options['default_session'])) {
-			$client = null;
-
+            
 			if (false == $mink->hasSession($options['default_session'])) {
 				if (isset($sessionsOptions[$options['default_session']])) {
 					$currentSessionOptions = $sessionsOptions[$options['default_session']];
@@ -129,24 +84,22 @@ class FeatureContext extends \Behat\MinkExtension\Context\MinkContext implements
 					}
 
 					$client = new \Selenium\Client($currentSessionOptions['host'], $port);
-					$driver = new \Behat\Mink\Driver\Selenium2Driver($options['default_session']);
 				} else {
 					$client = null;
 				}
 
 				$driver = new \Ptbfw\Selenium2Driver\Selenium2Driver();
-				//$session = new \Behat\Mink\Session($driver);
 				$session = new \Ptbfw\Mink\Session($driver);
 				$session->start();
-				$session->visit($options['base_url']);
 				$mink->registerSession($options['default_session'], $session);
 			}
 		} else {
 			throw new Exception('goutte session not implemented');
 			// @TODO
-			$driver = new \Behat\Mink\Driver\GoutteDriver();
 		}
+        
 		$mink->setDefaultSessionName($options['default_session']);
+        $mink->getSession()->visit($options['base_url']);
 	}
 
 
